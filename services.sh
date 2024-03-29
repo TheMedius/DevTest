8f1d441f4695f45b8babc95f1c4de503e954d6e1
Ranjan Singh <129151470+DevOps-TheMedius@users.noreply.github.com>
2024-03-29 21:29:21 +0530

    Update services.sh

--- services.sh
+++ services.sh
@@ -2,21 +2,21 @@
 echo "----------------------------------------------NGINX------------------------------------------------------------------------"
 sudo systemctl start nginx
 sudo systemctl status nginx
-echo "----------------------------------------------CELERY------------------------------------------------------------------------"
-sudo docker start celerymain
-celery=$(sudo -S docker ps -q  --filter ancestor=devtest)
-sudo docker update --restart unless-stopped $celery
-sudo docker update --restart unless-stopped $voicebot
-sudo docker ps
+#echo "----------------------------------------------CELERY------------------------------------------------------------------------"
+#sudo docker start celery
+#celery=$(sudo -S docker ps -q  --filter ancestor=devtest)
+#sudo docker update --restart unless-stopped $celery
+#sudo docker update --restart unless-stopped $voicebot
+#sudo docker ps
 #echo "----------------------------------------------DAPHNE------------------------------------------------------------------------"
 #sudo systemctl start daphne
 #sudo systemctl status daphne
 echo "----------------------------------------------GUNICORN----------------------------------------------------------------------"
 sudo systemctl start gunicorn
 sudo systemctl status gunicorn
-echo "----------------------------------------------CELERY-----------------------------------------------------------------------"
-sudo docker exec celerymain celery -A medius.celery_app purge -f
-sudo docker restart $(sudo -S docker ps -q  --filter ancestor=devtest)
-sudo docker ps
-echo "----------------------------------------------CLEAR QUEUE------------------------------------------------------------------"
-sudo docker exec celerymain celery -A medius.celery_app purge -f
+#echo "----------------------------------------------CELERY-----------------------------------------------------------------------"
+#sudo docker exec celerymain celery -A medius.celery_app purge -f
+#sudo docker restart $(sudo -S docker ps -q  --filter ancestor=devtest)
+#sudo docker ps
+#echo "----------------------------------------------CLEAR QUEUE------------------------------------------------------------------"
+#sudo docker exec celerymain celery -A medius.celery_app purge -f
