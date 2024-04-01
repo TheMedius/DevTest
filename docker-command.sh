3163a877385efc22e3c4a218bb8ef0f15ccc0988
Ranjan Singh <129151470+DevOps-TheMedius@users.noreply.github.com>
2024-04-01 13:29:10 +0530

    Update docker-command.sh

--- docker-command.sh
+++ docker-command.sh
@@ -7,5 +7,5 @@ sudo docker build -f DockerFileCelery . -t testcelery:latest
 # Build celery beat
 #sudo docker build -f DockerFileCeleryBeat . -t testcelery-beat:latest
 
-sudo docker run -d --restart=on-failure:5 --network=host -v /var/lib/jenkins/workspace/Backend/:/app/ -v /tmp/:/tmp/ --name celery testcelery
+sudo docker run -d --restart=on-failure:5 --network=host -v /var/lib/jenkins/workspace/DevTest/:/app/ -v /tmp/:/tmp/ --name celery testcelery
 #sudo docker run -d --restart=on-failure:5 --network=host -v /var/lib/jenkins/workspace/Backend/:/app/ -v /tmp/:/tmp/ testcelery-beat
